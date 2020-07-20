bd01f6c09b3c09d0014f31b5b95672b50d00efee
palak273 <38990907+palak273@users.noreply.github.com>
2020-07-20 15:50:44 +0530

    commit

--- src/main/java/com/javaApp/SampleApp.java
+++ src/main/java/com/javaApp/SampleApp.java
@@ -1,7 +1,12 @@
 package com.javaApp;
 
+import java.util.Scanner;
+
 public class SampleApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
         System.out.println("Hello! This is a java app");
+        System.out.println("Number entered = "+a);
     }
 }
